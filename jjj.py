ae1362ce9db552d93aaf3a3da2e18480aff784c8
Madhu3432 <139035784+Madhu3432@users.noreply.github.com>
2023-11-03 08:04:08 +0530

    Add files via upload

--- jjj.py
+++ jjj.py
@@ -1,2 +1,13 @@
+function convertAgeToDays(age) {
+  if (age < 0) {
+    return "Age cannot be negative!";
+  }
 
-y = 20
+  const daysInYear = 365.25; // accounting for leap years
+  const days = age * daysInYear;
+
+  return `Your age in days is approximately ${days.toFixed(2)} days.`;
+}
+
+const age = parseFloat(prompt("Enter your age:"));
+console.log(convertAgeToDays(age));
